9083acbde1f6940db635101ffc04c602c8d35210
Fonseca Naranjo Carlos <Carlos.Fonseca+Suracl@externos.afpcapital.cl>
2025-02-02 19:41:12 -0500

    Se realiza las funcionalidades de los formularios de cotizacion

--- src/app/components/terrain-features/terrain-features.component.ts
+++ src/app/components/terrain-features/terrain-features.component.ts
@@ -1,9 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-terrain-features',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass, NgIf],
   templateUrl: './terrain-features.component.html',
   styleUrl: './terrain-features.component.css',
 })
@@ -11,12 +18,39 @@ export class TerrainFeaturesComponent {
   @Output() toFirstStep = new EventEmitter<boolean>();
   @Output() toThirdStep = new EventEmitter<boolean>();
 
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.form = this.fb.group({
+      area: ['', [Validators.required, Validators.min(0)]],
+      inclination: ['', [Validators.required]],
+      rocky_soil: ['', [Validators.required]],
+    });
+  }
+
   navigateToFirstStep(): void {
-    console.log('navigateToFirstStep');
     this.toFirstStep.emit(true);
   }
 
   navigateToThirdStep(): void {
-    this.toThirdStep.emit(true);
+    if (this.form.valid) {
+      this.toThirdStep.emit(true);
+    } else {
+      this.markFormGroupTouched(this.form);
+    }
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup): void {
+    Object.values(formGroup.controls).forEach((control) => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
   }
 }
